7724ad17a470749a93ca2df829ec784a22d44106
denisoetken <denisoetken@hotmail.de>
2016-09-28 11:28:35 +0200

    Initialisierer

--- src/Initialisierer.java
+++ src/Initialisierer.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by doetken on 28.09.2016.
@@ -10,16 +15,45 @@ import java.util.Scanner;
 public class Initialisierer {
 
     public static void main(String[] args) {
-        String url="jdbc:postgresql://localhost/wetter";
-        String user="postgres";
-        String pass="password";
-
-        try (Connection con= DriverManager.getConnection(url, user, pass))
-        {
-            con.
-        } catch (SQLException e){
-            System.err.print("Fehler: " + e.getLocalizedMessage()+"("+e.getSQLState()+")");
+        String url = "jdbc:postgresql://localhost/wetter";
+        String user = "postgres";
+        String pass = "password";
+        String fileName = "";
+        for (String befehl : Initialisierer.readSQLFile("./Unterlagen/wetter-db/wetterdaten_tabellen.sql")) {
+            System.out.println(befehl);
         }
+//        Initialisierer.readSQLFile();
+
+        try (Connection con = DriverManager.getConnection(url, user, pass); Statement st = con.createStatement()) {
+
+        } catch (SQLException e) {
+            System.out.println("Fehler: " + e.getLocalizedMessage() + " (" + e.getSQLState() + ")");
+        }
+
+    }
+
+    private static List<String> readSQLFile(String fileName) {
+        List<String> ergebnis = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(fileName))) {
+            String zeile = "";
+            String befehl = "";
+
+            while ((zeile = rd.readLine()) != null) {
+                befehl += zeile;
+                if (befehl.endsWith(";")) {
+                    ergebnis.add(befehl);
+                    befehl = "";
+                }
+                befehl.trim();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ergebnis;
     }
 
 }
